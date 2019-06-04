f3b630963c0649cda836afb891f91e994c6af393
klanmiko <eternity01033@gmail.com>
2019-06-04 15:03:15 -0700

    update search backend for arrays and indexing

--- js/searchBackend.js
+++ js/searchBackend.js
@@ -1,10 +1,10 @@
-function initSearch(link, categories) {
-  function loadBackend(link) {
+function initSearch(link, categories, indexBuilder) {
+  function loadBackend() {
     return fetch(new Request(link))
         .then((response) => response.json())
         .then((json) => {
           for(let title in json) {
-            if(!categories.includes(json[title].category)) {
+            if(json[title].category && !categories.includes(json[title].category)) {
               delete json[title]
             }
           }
@@ -17,19 +17,25 @@ function initSearch(link, categories) {
     return new Promise((resolve, reject) => {
       window.addEventListener('DOMContentLoaded', () => {
         elasticlunr.clearStopWords();
-        let builder = elasticlunr(function(){
-          this.setRef('title')
-          this.addField('description')
-          this.addField('author')
-          this.addField('title')
-        })
+        let builder = elasticlunr(indexBuilder)
         resolve(builder);
       })
     });
   }
 
-  return Promise.all([loadBackend(link), getLunrBuilder()]).then(([data, builder]) => {
-    Object.values(data).forEach(builder.addDoc.bind(builder));
-    return builder
+  return Promise.all([loadBackend(), getLunrBuilder()]).then(([data, builder]) => {
+    Object.values(data).map((doc) => {
+        Object.keys(doc).forEach((key) => {
+          if(Array.isArray(doc[key])) {
+            doc[key] = doc[key].join(" ")
+          }
+        })
+        return doc;
+      }
+    ).forEach(builder.addDoc.bind(builder));
+    return {
+      searchData: data,
+      index: builder
+    }
   })
 }
